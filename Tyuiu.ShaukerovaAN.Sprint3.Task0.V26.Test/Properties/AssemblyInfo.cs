e323dca4a070ab0427031ced963ff74e0c5d976a
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-10-18 22:40:45 +0500

    Task0

---
+++ Tyuiu.ShaukerovaAN.Sprint3.Task0.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShaukerovaAN.Sprint3.Task0.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShaukerovaAN.Sprint3.Task0.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("affb78a5-83a0-4d7b-ad6c-b004f0152d25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
